c8c8d342bacff790f38df04da905d6e3bd919166
sagan <camjanssen120@gmail.com>
2024-05-06 15:17:21 -0700

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub enum GGSTDLError {
 }
 
 impl Display for GGSTDLError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             GGSTDLError::UnknownCharacter => write!(f, "Unknown character"),
             GGSTDLError::UnknownMove => write!(f, "Unknown move"),
